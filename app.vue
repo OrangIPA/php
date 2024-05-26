0b0bbb6854fa3fba15ad619dbba0e99e1a6e7c80
Bahana Alana Khansa <alanabahana102@gmail.com>
2024-05-26 22:47:59 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import '~/assets/css/main.css'
+</script>
+
+<template>
+  <body class="bg-zinc-800">
+    <NuxtPage />
+  </body>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center">
+    <main
+      class="grid grid-cols-[minmax(0px,_2fr)_10px_minmax(0px,_1fr)] gap-x-9"
+    >
+      <div class="flex flex-col items-end">
+        <div
+          class="text-[11rem] leading-none text-teal-500 decoration-8 hover:underline underline-offset-8"
+        >
+          Alan
+        </div>
+        <div class="text-3xl text-neutral-200">
+          And i'm a <span class="text-teal-500 hover:underline underline-offset-4 decoration-4 text-4xl">Software developer</span>
+        </div>
+      </div>
+      <div class="bg-neutral-200"></div>
+      <div class="flex w-fit flex-col items-start justify-center gap-2">
+        <a href="/" class="text-2xl text-neutral-200 hover:underline hover:text-teal-500">Linkedin</a>
+        <a href="/" class="text-2xl text-neutral-200 hover:underline hover:text-teal-500">Github</a>
+        <a href="/" class="text-2xl text-neutral-200 hover:underline hover:text-teal-500">Instagram</a>
+        <a href="/" class="text-2xl text-neutral-200 hover:underline hover:text-teal-500">Blogs</a>
+      </div>
+    </main>
+  </div>
+</template>
